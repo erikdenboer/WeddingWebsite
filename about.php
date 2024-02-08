d474a97317a3656fea3f2fe27e267779a8fbf3b2
Erik den Boer <erikdenboer92@gmail.com>
2024-02-08 23:16:14 +0100

    Content creating  - Added info to About the Wedding page  - Added info to Contact page

--- about.php
+++ about.php
@@ -2,8 +2,120 @@
 
 <!--Main Content Start -->
 		<main>
-            <h2>About the wedding</h2>
-            <p>Hello world! This is the info page for our wedding!</p>
+            <h1>About the wedding</h1>
+            <p>Hello dear guests! Here, you will find all manner of practical info about the wedding <!--couple, -->location and schedule. 
+            If you have any further questions, please head over to the <a href="contact.php">Contact page</a> to find out who to ask and how to reach them.
+            </p>
+            
+            <h3>RSVP<h3>
+            <p>Please let us know if you'll join us on this festive day! Do this before date by clicking on the link below, then we'll make sure to save a seat for you. 
+                Do you have any dietary restrictions or allergies? Please let us know with your RSVP.</p>
+
+            <h3>Dresscode</h3>
+            <p>The dresscode for our wedding is formal. This is an invitation for you to dress up! So think dresses for the women and nice trousers or a suit for the men.</p>
+            
+            <h3>Speeches<h3>
+            <p>Would you like to give a speech? We would love that! Please let Astrid know, she will reserve a moment for you in the planning of the day.</p>
+
+            <h3>Unplugged wedding</h3>
+            <p>We've hired a professional photographer to capture all special moments of this day. We would like you to enjoy it all, 
+                so we ask you to keep your phone put away as much as possible during the whole day.</p>
+
+            <h3>Gift tip</h3>
+            <p>We would love to go on a honeymoon later this year. You would make us very happy with a contribution to make this trip even more special.</p>
+            
+            <h1>About the location<h1>
+
+            <h3>Restaurant De Mof</h3>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2450.514704099919!2d5.412400776202065!3d52.1067631667427!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c645065216741f%3A0x618b5b19dd95bbd7!2sRestaurant%20De%20Mof!5e0!3m2!1sen!2sde!4v1706651816697!5m2!1sen!2sde" 
+                width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+
+            <h3>Recommended means of travel<h3>
+            <p>Restaurant De Mof is easy to reach by car, situated 5-10 minutes by car from the highway A12, exit 22 towards Maarsbergen. There is plenty of parking available on the terrain,
+                making this one of the easiest ways of reaching the venue. It is also possible to come by train to Amersfoort Central Station and then transferring to bus 80,
+                which will drop you right outside the venue in about 20 minutes. However, note that the return bus runs only once an hour on Saturday evenings.
+                For additional flexibility, or if you want a real taste of the Dutch way of life, it is possible to rent bikes at the train station, after which it's a 30-minute ride to the venue.
+            </p>
+
+            <h3>Hotels nearby</h3>
+            <p>For those wishing to stay the night nearby, there are several options:
+                <ul>
+                    <li><a href="https://www.hotelleusden.nl/">Van der Valk Hotel Amersfoort Leusden</a>
+                     - a well-known Dutch hotel chain. The happy couple will spend the wedding weekend in one of their hotels, actually!</li>
+                    <li><a href="https://www.fletcherhotelamersfoort.nl/en/">Fletcher Hotel-Restaurant Amersfoort</a>
+                     - a bit cheaper than the Van der Valk hotels, but a solid choice if you just want a good room for a good price.</li>
+                    <li><a href="https://www.europarcs.com/holiday-parks/the-netherlands/utrecht/de-utrechtse-heuvelrug">Europarcs De Utrechtse Heuvelrug</a>
+                     - rent a chalet in a rural location. Perfect for groups or those who prefer the Dutch countryside over a regular hotel.</li>
+                    <li>There are also several small, mostly family-run B&B's in rural settings nearby, such as 
+                     <a href="https://www.maarsbergseschuurtje.nl/">'t Maarsbergse Schuurtje'</a>.</li>
+                </ul>
+                Unable to find something you like? Contact the groom, or just call Restaurant De Mof - they certainly know which places close by are worth checking out!</p>
+
+            <h1>About the schedule</h1>
+            <p>Create table for the schedule:
+                <table>
+                    <tr>
+                        <th>Time</th>
+                        <th>Activity</th>
+                        <th>Notes</th>
+                    </tr>
+                    <tr>
+                        <td>13:00</td>
+                        <td>Reception of guests</td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>14:00</td>
+                        <td>Depart to ceremony location</td>
+                        <td>There's no other way to get there, so be on time!!!</td>
+                    </tr>
+                    <tr>
+                        <td>14:30</td>
+                        <td>Wedding ceremony</td>
+                        <td>Takes place in a nearby forest</td>
+                    </tr>
+                    <tr>
+                        <td>15:30</td>
+                        <td>Toast</td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>15:45</td>
+                        <td>Taking group photos</td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>16:15</td>
+                        <td>Return to De Mof</td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>16:45</td>
+                        <td>Cake cutting</td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>18:00</td>
+                        <td>Dinner</td>
+                        <td>Three-course meal based on choices in RSVPs</td>
+                    </tr>
+                    <tr>
+                        <td>20:00</td>
+                        <td>DJ</td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>22:30</td>
+                        <td>Bride and groom leave</td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>23:00</td>
+                        <td>End of day</td>
+                        <td></td>
+                    </tr>
+            </p>
+            
         </main>
 	</div>
     
--- contact.php
+++ contact.php
@@ -2,8 +2,22 @@
 
 <!--Main Content Start -->
 		<main>
-            <h2>Contact</h2>
-            <p>Hello world! Location details, useful phone numbers and a map... what more could you ask for?</p>
+            <h1>Contact</h1>
+            <p>In case you have any questions before the wedding day, you can contact the bride or groom:
+                <ul>
+                    <li>Zaneera: +49 176 3746 8188</li>
+                    <li>Erik: +49 172 169 9067</li>
+            </ul>
+            
+            <h2>Wedding coordinator</h2>
+            <p>Wedding planner Astrid is our professional wedding coordinator who is in charge of the programme during the day. 
+            If you have any requests please contact her by email: astrid@mooistemomentweddings.nl. Make sure to mention it concerns our wedding. 
+            If anything comes up during the day, she's also your first point of contact.</p>
+
+            <h2>Venue</h2>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2450.514704099919!2d5.412400776202065!3d52.1067631667427!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c645065216741f%3A0x618b5b19dd95bbd7!2sRestaurant%20De%20Mof!5e0!3m2!1sen!2sde!4v1706651816697!5m2!1sen!2sde" 
+                width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    
         </main>
 	</div>
 <!--Main Content Start -->
